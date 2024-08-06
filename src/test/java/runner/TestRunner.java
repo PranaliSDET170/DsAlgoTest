c6b32d1dccd090cefe6ea07f165e0b56bc2619dd
PranaliSDET170e <pranali.dige@gmail.com>
2024-08-06 00:14:24 -0500

    Queue feature file template complete

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,27 @@
+package runner;
+
+import org.testng.annotations.DataProvider;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+//@RunWith(Cucumber.class) //Junit execution
+
+	@CucumberOptions(
+			plugin = {"pretty", "html:target/TestRusult.html"}, //reporting purpose
+			monochrome=true,  //console output colour
+			tags = "@tag", //tags from feature file
+			features = {"src/test/resources/features/Queue.feature"}, //location of feature files
+			glue= "stepdefination") //location of step definition files
+
+
+	public class TestRunner extends AbstractTestNGCucumberTests{
+		
+		@Override
+	    @DataProvider(parallel = false)
+	    public Object[][] scenarios() {
+					
+			return super.scenarios();	
+	    }
+
+	
+}
