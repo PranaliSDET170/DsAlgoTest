e892c4586fdb2d72d9a0ee21fc86db3b27148701
PranaliSDET170e <pranali.dige@gmail.com>
2024-08-07 22:57:05 -0500

    Wit time reduced to 3 sec

--- src/test/java/stepdefinition/QueueSteps.java
+++ src/test/java/stepdefinition/QueueSteps.java
@@ -1,10 +1,8 @@
 package stepdefinition;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.After;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -13,11 +11,6 @@ public class QueueSteps {
 	
 	WebDriver driver;
 	
-	@After
-	public void closeWebPage() throws InterruptedException {
-		Thread.sleep(10000);
-		LoginSteps.getDriver().close();
-	}
 
 	@Given("user is on homepage")
 	public void user_is_on_homepage() {
@@ -32,6 +25,12 @@ public class QueueSteps {
 	@Then("user is navigated to queue homepage")
 	public void user_is_navigated_to_queue_homepage() {
 
+	}
+
+	@After
+	public void closeWebPage() throws InterruptedException {
+		Thread.sleep(3000);
+		LoginSteps.getDriver().close();
 	}
 
 }
