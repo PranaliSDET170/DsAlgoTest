6ce00383e9aa437d0c4fba2f692b7b3411e5115c
PranaliSDET170e <pranali.dige@gmail.com>
2024-08-13 23:03:09 -0500

    Formatting done

--- src/test/java/stepdefinition/LoginSteps.java
+++ src/test/java/stepdefinition/LoginSteps.java
@@ -19,29 +19,27 @@ import io.cucumber.java.en.When;
 public class LoginSteps {
 
 	private static WebDriver driver;
-	
+
 	public static WebDriver getDriver() {
 		return driver;
 	}
-	
+
 	@Given("user is navigated to login page using {string} browser")
 	public void user_is_navigated_to_login_page_using_browser(String browserName) {
 		if (browserName.equals("Chrome")) {
 			driver = new ChromeDriver();
 		} else if (browserName.equals("Firefox")) {
 			driver = new FirefoxDriver();
-		}else if (browserName.equals("Safari")) {
+		} else if (browserName.equals("Safari")) {
 			driver = new SafariDriver();
 		}
-		
+
 		driver.get("https://dsportalapp.herokuapp.com/login");
 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
 
-		
 	}
-
 
 	@Given("user is on login page")
 	public void user_is_on_login_page() {
@@ -67,15 +65,15 @@ public class LoginSteps {
 		driver.findElement(By.xpath("//input[4]")).click();
 
 	}
-	
+
 	@Then("user is successfully logged in")
 	public void user_is_successfully_logged_in() {
 		Assert.assertEquals(driver.findElement(By.className("alert")).getText(), "You are logged in");
 	}
-	
+
 	@Given("user is on homepage")
 	public void user_is_on_homepage() {
-		//Assert.assertEquals(<ActualValueReturnedByDriver>, <ExpectedValue>);
+		// Assert.assertEquals(<ActualValueReturnedByDriver>, <ExpectedValue>);
 		Assert.assertEquals(driver.getTitle(), "NumpyNinja");
 
 	}
