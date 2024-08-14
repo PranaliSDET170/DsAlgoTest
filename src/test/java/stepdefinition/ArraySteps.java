bdd05b8da0fdd994c05f6052d70fefdf127c6cf4
[Nanditawalke] <nandita.s.walke@gmail.com>
2024-08-13 20:50:12 -0400

    Arrays in python completed

--- src/test/java/stepdefinition/ArraySteps.java
+++ src/test/java/stepdefinition/ArraySteps.java
@@ -9,17 +9,30 @@ import io.cucumber.java.en.When;
 
 public class ArraySteps {
 	WebDriver driver = LoginSteps.getDriver();
+
 	@When("user clicks on get started button in Array module")
 	public void user_clicks_on_get_started_button_in_array_module() {
 		driver.findElement(By.xpath("//a[@href='array']")).click();
-	   
+
 	}
 
 	@Then("user is navigated to Array homepage")
 	public void user_is_navigated_to_array_homepage() {
-	    
 		Assert.assertEquals(driver.getTitle(), "Array");
+
 	}
 
+	@Then("user clicks on Arrays in Python link")
+	public void user_clicks_on_arrays_in_python_link() {
+		driver.findElement(By.linkText("Arrays in Python")).click();
+
+	}
+
+	@Then("user is navigated to Arrays in Python homepage")
+	public void user_is_navigated_to_array_in_python_homepage() {
+
+		Assert.assertEquals(driver.getTitle(), "Arrays in Python");
+
+	}
 
 }
--- src/test/resources/features/Array.feature
+++ src/test/resources/features/Array.feature
@@ -12,3 +12,15 @@ Feature: This feature file contain scenario for the module Array
     Given user is on homepage
     When user clicks on get started button in Array module
     Then user is navigated to Array homepage
+
+  @ArrayInPython
+  Scenario: verify Array In Python
+    Given user is on homepage
+    When user clicks on get started button in Array module
+    Then user is navigated to Array homepage
+    And user clicks on Arrays in Python link
+    Then user is navigated to Arrays in Python homepage
+    And user clicks on Try here button
+    And user writes valid python code
+    And user clicks on Run button
+    Then user is able to see the output in the console
