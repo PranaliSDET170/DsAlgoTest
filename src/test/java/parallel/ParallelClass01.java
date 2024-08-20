e45fc41e24c14d7a8a03e9b7978a53b74ddb3990
[Nanditawalke] <nandita.s.walke@gmail.com>
2024-08-20 15:30:43 -0400

    parallel testing completed

---
+++ src/test/java/parallel/ParallelClass01.java
@@ -0,0 +1,27 @@
+package parallel;
+
+import org.testng.annotations.Test;
+
+public class ParallelClass01 {
+//A thread is an independent path of execution within a program
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test01() {
+		System.out.println("I am in test01_class@01 - ID:" + Thread.currentThread().getId());
+		
+	}
+	
+	@Test
+	public void test02() {
+		System.out.println("I am in test02_Class01");
+		
+		System.out.println("I am in test01_class@02 - ID:" + Thread.currentThread().getId());
+	}
+	
+	@Test
+	public void test03(){
+		System.out.println("I am in test03_Class01");
+		System.out.println("I am in test03_class@01 - ID:" + Thread.currentThread().getId());
+		
+	}
+}
