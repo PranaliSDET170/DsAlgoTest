a8a515ec16976ba54a1370eaeceea4abaca4a7ba
Pranali Dige <pranali.dige@gmail.com>
2024-08-20 16:39:23 -0500

    Fix failing test

--- src/test/java/hooks/CucumberHooks.java
+++ src/test/java/hooks/CucumberHooks.java
@@ -1,11 +1,30 @@
-package utilities;
+package hooks;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+
 import com.aventstack.extentreports.Status;
 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import utilities.ExtentManager;
+import utilities.WebDriverManager;
+
 public class CucumberHooks {
+
+	private WebDriver driver;
+
+	public CucumberHooks(WebDriverManager driverManager) {
+		this.driver = driverManager.getDriver();
+	}
+
+	@AfterStep
+	public void afterStep() {
+//		System.out.println("Session Id is: " + ((RemoteWebDriver) driver).getSessionId());
+	}
 
 	@Before
     public void beforeScenario(Scenario scenario) {
@@ -15,6 +34,8 @@ public class CucumberHooks {
 
     @After
     public void afterScenario(Scenario scenario) {
+    	driver.close();
+    	
         if (scenario.isFailed()) {
             ExtentManager.getTest().log(Status.FAIL, "Scenario failed");
         } else {
@@ -22,5 +43,5 @@ public class CucumberHooks {
         }
         ExtentManager.getInstance().flush();
     }
-	
+
 }
