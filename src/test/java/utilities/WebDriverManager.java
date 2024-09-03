10b8da55d7d123337effe7bc2eb12b62dd1aa036
[Nanditawalke] <nandita.s.walke@gmail.com>
2024-09-03 09:07:59 -0400

    pom.xml completed

---
+++ src/test/java/utilities/WebDriverManager.java
@@ -0,0 +1,40 @@
+package utilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.Assert;
+
+public class WebDriverManager {
+	private final WebDriver driver;
+
+	/*
+	 * Invoked by dependency injection framework: scucumber-picocontainer
+	 */
+	public WebDriverManager() {
+		String browserName = ConfigPropertiesReader.getPropertyValue("browserKey");
+
+		// Initialize local variable based on property
+		if (browserName.equalsIgnoreCase("Chrome")) {
+			this.driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("FireFox")) {
+			this.driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("Safari")) {
+			this.driver = new SafariDriver();
+		} else {
+			this.driver = null;
+			Assert.fail("Invalid Browser Name");
+		}
+		
+		System.out.println("New Driver Created!!!");
+	}
+
+	/*
+	 * To return active driver instance
+	 */
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+
+}
